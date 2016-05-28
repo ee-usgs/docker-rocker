99b7a4e4e0e779a438c0078cd23f0e6036091d1f
Eric Everman <eeverman@usgs.gov>
2016-05-28 15:33:42 -0500

    * Added completions for docker commands (the commands, not arguments) * Upgraded the readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # docker-rocker
 Docker CLI utils for Linux and OS X
+
+## Main Features
+
+### Completions
+'docker' commands will suggest auto completions.  For instance, typing:
+	docker ne[tab]
+will complete to:
+	docker network
+continuing with
+	docker network cr[tab]
+will complete to:
+	docker network create
+### Aliases
+The docker command is aliased as 'dock' and completion will work as expected.
+...More to come
+### Shortcuts, aliases and completiongs for common commands sequences
+The common command:
+	docker exec -it [instance name] /bin/bash
+Can be shortened to just:
+	docker-bash [name with completion of running instances]
+or even just one of these aliases:
+	dock-bash [name]
+	doba [name]
--- docker-rocker.sh
+++ docker-rocker.sh
@@ -28,3 +28,28 @@ alias dock-bash=dockBash
 alias docker-bash=dockBash
 
 complete -F _dockBash dockBash doba dock-bash docker-bash
+
+_docker() {
+	local cur=${COMP_WORDS[COMP_CWORD]}
+	local prev=${COMP_WORDS[COMP_CWORD-1]}
+
+	case "$prev" in
+	network)
+    	COMPREPLY=( $( compgen -W "connect create disconnect inspect ls rm" -- $cur ) )
+    	return 0
+    	;;
+	volume)
+    	COMPREPLY=( $( compgen -W "create inspect ls rm" -- $cur ) )
+    	return 0
+    	;;
+    *)
+		local cmds="daemon attach build commit cp create diff events exec export history images import info inspect kill load login logout logs network pause port ps pull push rename restart rm rmi run save search start stats stop tag top unpause update version volume wait"
+		COMPREPLY=( $(compgen -W "$cmds" -- $cur) )
+		;;
+	esac
+}
+
+# docker aliases
+alias dock=docker
+
+complete -F _docker docker dock
